075010c0eb049f8ec8704fca9a95bb9878085f2d
Sandeep Mewada <144431655+Sandeep6264@users.noreply.github.com>
2024-09-30 12:56:51 +0530

    Add files via upload

---
+++ src/components/cart/CartBuyButton.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+function CartBuyButton() {
+	const buy = () => {
+		
+	};
+	return (
+		<button
+			onClick={buy}
+			className="btn btn-success d-block w-100 fw-bold mt-3"
+		>
+			Buy Now
+		</button>
+	);
+}
+export default CartBuyButton;
